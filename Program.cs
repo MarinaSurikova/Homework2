0e2cd70ce6fbe5d741301806c466a7cae34c06f3
Marina Surikova <marina.capalina@mail.ru>
2022-02-10 07:39:53 +0300

    add Task_10

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+int n = new Random().Next(100,1000);
+Console.WriteLine("Случайное число: " +n);
+int result = n %100 /10;
+Console.WriteLine("Вторая цифра числа="+ result);
